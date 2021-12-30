48ae5655e12c687050f551071e2e8449e90caf03
CurlyMoo <curlymoo1@gmail.com>
2021-12-30 22:38:57 +0100

    Fix travis log limit

--- ci/travis-script.sh
+++ ci/travis-script.sh
@@ -9,26 +9,26 @@ if [[ ${PLATFORM} == "Unix" ]]; then
         -DCMAKE_CXX_FLAGS="-DWEBSERVER_MAX_SENDLIST=10 -DWEBSERVER_SENDLIST_BUFSIZE=0" ..
     make
 
-    ./start
+    ./start 2> 1.log && tail 1.log
 
     cmake -DCOVERALLS=ON \
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_CXX_FLAGS="-DWEBSERVER_MAX_SENDLIST=0 -DWEBSERVER_SENDLIST_BUFSIZE=32000" ..
     make
 
-    ./start
+    ./start 2> 2.log && tail 2.log
 
     cmake -DCOVERALLS=ON \
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_CXX_FLAGS="-DWEBSERVER_MAX_SENDLIST=10 -DWEBSERVER_SENDLIST_BUFSIZE=32000" ..
     make
 
-    ./start
+    ./start 2> 3.log && tail 3.log
 
     cmake -DCOVERALLS=ON \
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_CXX_FLAGS="-DWEBSERVER_MAX_SENDLIST=0 -DWEBSERVER_SENDLIST_BUFSIZE=0" ..
     make
 
-    ./start && exit 0;
+    ./start 2> 4.log && tail 4.log && exit 0;
 fi
